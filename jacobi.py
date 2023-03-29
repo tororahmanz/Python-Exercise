46eb0134aec383fb8dad3c0a84ed70e7f3418611
Toro Rahman Aziz <toro.aziz@pln.co.id>
2023-03-29 14:40:15 +0700

    Penyempurnaan Jacobi

--- jacobi.py
+++ jacobi.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def jacobi_method(A, b, x_init, tol=10e-6, max_iter=100):
+def jacobi_method(A, b, x_init, tol=10e-7, max_iter=100):
     n = len(A)
     x = x_init
     x_new = np.zeros(n)
@@ -11,11 +11,12 @@ def jacobi_method(A, b, x_init, tol=10e-6, max_iter=100):
             for j in range(n):
                 if i != j:
                     s += A[i, j] * x[j]
-            x_new[i] = np.round((b[i] - s) / A[i, i],5)
+            x_new[i] = (b[i] - s) / A[i, i]
 
         print(f"Iterasi ke-{k+1}: {x_new}")
         
-        if np.linalg.norm(x_new - x) < tol:
+        #if np.linalg.norm(x_new - x) < tol:
+        if np.all(np.abs(x_new - x) < tol):
             print(f"Konvergensi tercapai pada iterasi ke-{k+1}")
             return x_new
         x = x_new.copy()
@@ -34,5 +35,5 @@ if __name__ == "__main__":
     x_init = np.array(list(map(float, input("Masukkan vektor solusi awal (pisahkan dengan spasi): ").split())))
     
     x = jacobi_method(A, b, x_init)
-    print("Solution:", np.round(x,3))
+    print("Solution:", np.round(x,6))
     #print("Residual:", np.linalg.norm(A @ np.round(x,3) - b))
